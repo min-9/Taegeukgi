72a766b9e1213c30f3dea1d066be9028d1834184
Pandamin9 <34941024+kmingu07315@users.noreply.github.com>
2021-03-05 14:50:05 +0900

    Delete Taegeukgi directory

--- Taegeukgi/__init__.py
+++
@@ -1,6 +0,0 @@
-from .background import *
-from .yin_yang import *
-from .sky import *
-from .earth import *
-from .water import *
-from .fire import *
--- Taegeukgi/background.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-w, h = 600, 400
-
-bg = Image.new('RGB', (w, h), (241, 241, 241))
