98d5829d094497a4aa48ec93e7e1f6647e7b986b
Pandamin9 <34941024+kmingu07315@users.noreply.github.com>
2021-03-05 14:52:28 +0900

    Add files via upload

---
+++ Taegeukgi/background.py
@@ -0,0 +1,5 @@
+from PIL import Image
+
+w, h = 900, 600
+
+tgg = Image.new('RGB', (w, h), (241, 241, 241))
---
+++ Taegeukgi/fire.py
@@ -0,0 +1,14 @@
+from PIL import Image, ImageDraw
+from .background import h, tgg
+
+# 이(리) = 불 = 결실
+fire = Image.new('RGB', (h // 4, h // 6), color=(14, 14, 14))
+draw = ImageDraw.Draw(fire)
+
+draw.rectangle((0, h * 2 // 48, h // 4, h * 3 // 48), fill=(241, 241, 241))
+draw.rectangle((0, h * 5 // 48, h // 4, h * 6 // 48), fill=(241, 241, 241))
+draw.rectangle((h * 11 // 96, h * 3 // 48, h * 13 // 96, h * 5 // 48), fill=(241, 241, 241)) # 2
+
+fire = fire.rotate(90 + 33.69, expand=True, fillcolor=(241, 241, 241))
+
+tgg.paste(fire, (h * 22 // 96, h * 59 // 96))
